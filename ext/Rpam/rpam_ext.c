1a4237f56623a2d0c9f261f9b5d9c53c2bf262cc
Dallas Pool <dallas@cyphre.com>
2018-05-11 12:14:50 -0500

    testing return result
    Signed-off-by: Dallas Pool <dallas@cyphre.com>

--- ext/Rpam/rpam_ext.c
+++ ext/Rpam/rpam_ext.c
@@ -101,13 +101,13 @@ VALUE method_authpam(VALUE self, VALUE username, VALUE password, VALUE servicena
 	if ((result = pam_start(rpam_servicename, userinfo.name, &conv_info, &pamh))
             != PAM_SUCCESS) {
 
-        return Qfalse;
+        return result;
     }
     if ((result = pam_authenticate(pamh, PAM_DISALLOW_NULL_AUTHTOK))
            !=  PAM_SUCCESS) {
 
         pam_end(pamh, PAM_SUCCESS);
-        return Qfalse;
+        return result;
     }
 
    if ((result = pam_acct_mgmt(pamh, PAM_DISALLOW_NULL_AUTHTOK))
@@ -116,16 +116,16 @@ VALUE method_authpam(VALUE self, VALUE username, VALUE password, VALUE servicena
       // we'll allow the user to continue if the auth token is expired and handle password change within the applicaiton
       if((result != PAM_NEW_AUTHTOK_REQD)){
         pam_end(pamh, PAM_SUCCESS);
-        return Qfalse;
+        return result;
       }
    }
 
   if (result == PAM_NEW_AUTHTOK_REQD){
     pam_end(pamh, PAM_SUCCESS);
-    return -1;
+    return result;
   }else{
     pam_end(pamh, PAM_SUCCESS);
-    return Qtrue;
+    return result;
   }
 
 }
